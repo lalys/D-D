e106b17fc161c4c23d3831607ad038f66d313856
lalys <xion1337@morris.umn.edu>
2019-12-03 21:34:41 -0600

    Add new player button and fields "Does not work" Add new player functionality Show list of characters Try to add their views

--- main2.ctrl.js
+++ main2.ctrl.js
@@ -1,8 +1,24 @@
 angular.module('app').controller("MainController", function(){
-    this.title = 'D&D Character'
+    this.title = 'D&D Characters';
     this.searchInput = '';
-
-
-
+    this.shows = [
+        {
+            name: 'Laly',
+            type: 'View'
+        }
+    ];
+    this.stuff = [
+        {
+            equipment: 'stuff',
+            class: 'stuff',
+            ability: 'stuff',
+            race: 'stuff'
+        }
+    ];
+    this.new = {};
+    this.addPerson = function(){
+        this.shows.push(this.new);
+        this.new = {};
+    };
 
 });
